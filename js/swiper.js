e04ac275e30d469adcca4840218b576e747267b0
Grandmozer <svetoskover@gmail.com>
2024-03-18 21:48:36 +0500

    swiper

---
+++ js/swiper.js
@@ -0,0 +1,39 @@
+// Получаем элементы слайдера
+const slider = document.querySelector('.participants-swiper-desk');
+const prevButton = document.querySelector('.participants__button-prev');
+const nextButton = document.querySelector('.participants__button-next');
+const slides = Array.from(slider.querySelectorAll('.swiper-item'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+console.log(slides);
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
